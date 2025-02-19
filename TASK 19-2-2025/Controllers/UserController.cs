77d428b33adbccf8e04938189198c8eaeabaa795
surahailat <hailatsura@gmail.com>
2025-02-19 22:23:16 +0300

    Add project files.

---
+++ TASK 19-2-2025/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TASK_19_2_2025.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
